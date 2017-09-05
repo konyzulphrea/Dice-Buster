4165547c5d53c35607374791863d8d9a6fc83805
Kenneth CHU <polarhei@gmail.com>
2017-09-05 17:39:50 +0800

    The_Feeling
    No one wants refreshed often, so I make a prototype, and I shall need to add a textbox to fix the damned, let you see the step two first.

---
+++ padnconvert.js
@@ -0,0 +1,21 @@
+   function pad (str, max) {
+      return str.length < max ? pad("0" + str, max) : str;
+    }
+    
+    //alert(pad("5", 64));
+function dec2hex(str){ // .toString(16) only works up to 2^53
+    var dec = str.toString().split(''), sum = [], hex = [], i, s
+    while(dec.length){
+        s = 1 * dec.shift()
+        for(i = 0; s || i < sum.length; i++){
+            s += (sum[i] || 0) * 10
+            sum[i] = s % 16
+            s = (s - sum[i]) / 16
+        }
+    }
+    while(sum.length){
+        hex.push(sum.pop().toString(16))
+    }
+    return hex.join('')
+}
+
